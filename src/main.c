ceddca96d74a0858d8ee36884f031c6da73ebc8c
tim-BIOS <tim@binarysolutions.io>
2018-02-23 15:26:24 -0800

    [BSPS-28] Deleted “use full assert” section of code from main.c
    - deleted because never used

--- src/main.c
+++ src/main.c
@@ -230,14 +230,6 @@ void testCallback(void) {
 }
 
 
-
-
-
-
-
-
-
-
 /* attempts to get a command from the byte source that is the receive***Func pointers
  * returns true if a command was successfully read, false if it was not.
  * only modifies the commandInFlight if it returns true
@@ -519,26 +511,6 @@ void SystemClock_Config(void)
 }
 
 
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t *file, uint32_t line)
-{
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-#endif
 
 /**
   * @}
