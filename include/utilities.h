62e8eee299b6eeb27e55bd2ce81c20bf832b5b83
tim-BIOS <tim@binarysolutions.io>
2018-02-26 12:35:50 -0800

    [BSPS-28] Deleted all useless commented out code in utilities.c
    - Deleted because not used.

--- include/utilities.h
+++ include/utilities.h
@@ -26,12 +26,9 @@ size_t Utilities_packToBuffer32(uint8_t*, uint32_t, uint32_t);
 uint32_t Utilities_extract32(uint8_t*, uint32_t);
 uint16_t Utilities_extract16(uint8_t*, uint32_t);
 size_t Utilities_extractBuffer(uint8_t* outBuff, uint8_t* inBuff, size_t len);
-
 void Utilities_zeroize(uint8_t* d, uint32_t len);
-
 uint8_t Utilities_compareDigests(const uint8_t const *digestOne, const uint8_t const *digestTwo, const uint32_t len);
 int32_t Utilities_aesCtrCrypt(mbedtls_aes_context*, size_t, uint8_t nonce_counter[16], uint8_t*, uint8_t*);
-
 int Utilities_getRandomBuff(void* param, unsigned char* buff, size_t len);
 uint32_t Utilities_init(RNG_HandleTypeDef* hrng);
 
